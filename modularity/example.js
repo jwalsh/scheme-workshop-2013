99c63695b95562fef249d5fe58a24334a94a2b22
Jason Walsh <jwalsh@bluekai.com>
2013-11-13 07:06:07 -0800

    Structure

---
+++ modularity/example.js
@@ -0,0 +1,17 @@
+// Provide examples 
+
+function Cell() {
+
+    this.merge = function(a, b) {};
+    this.proc = function(f) {};
+}
+
+// Confirm 
+
+// Commutativity
+
+// Associativity
+
+// Idempotent
+
+// Monotonic
